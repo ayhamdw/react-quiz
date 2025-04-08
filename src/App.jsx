37f3c47f42c094cfff077a7c6b60541dc8da2c43
ayhamdw <s12029113@stu.najah.edu>
2025-04-08 13:17:24 +0200

    Add useReducer, add static data to fake server

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useReducer, useState } from "react";
 import "./App.css";
-import DateCounter from "./DateCounter";
 import Header from "./components/Header";
+import Main from "./components/Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
 
   return (
     <div className="app">
-      <main className="main">
+      <Header />
+      <Main>
         <p>1/15</p>
         <p>Question?</p>
-      </main>
-      <Header />
+      </Main>
     </div>
   );
 }
